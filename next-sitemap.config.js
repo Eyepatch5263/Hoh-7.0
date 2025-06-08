86391104766f52f1698ad8bf049dfb1fabe66c92
Eyepatch5263 <pratyushpragyey.com>
2025-06-09 01:30:18 +0530

    added robot.txt and sitemap.xml files for page crawling

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://hackonhills.com',
+  generateRobotsTxt: true, // will also generate robots.txt
+  sitemapSize: 7000,
+};
