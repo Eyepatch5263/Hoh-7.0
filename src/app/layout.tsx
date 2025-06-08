3121b0925b27d6bcfa3c9e2ce0b6fb467df6d7e9
Eyepatch5263 <pratyushpragyey.com>
2025-06-08 18:55:05 +0530

    seo improvements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,32 @@ export default function RootLayout({
               "@context": "https://schema.org",
               "@type": "Event",
               "name": "Hack On Hills - 7.0",
-              "logo":"/hoh2.webp",
+              "eventStatus":"https://schema.org/EventScheduled",
+              "location":{
+                "@type": "Place",
+                "name": "NIT HAMIRPUR",
+                "address": {
+                  "@type": "PostalAddress",
+                  "streetAddress": "Hamirpur",
+                  "addressLocality": "Hamirpur",
+                  "addressRegion": "Himachal Pradesh",
+                  "postalCode": "177005",
+                  "addressCountry": "India"
+                }
+              },
+              "image":[
+                "https://res.cloudinary.com/dvnrlqqpq/image/upload/v1749385680/img7_zheh58.webp",
+                "https://res.cloudinary.com/dvnrlqqpq/image/upload/v1749385678/img2_wreqs7.webp",
+                "https://res.cloudinary.com/dvnrlqqpq/image/upload/v1749385679/img1_lfcfmv.webp",
+                "https://res.cloudinary.com/dvnrlqqpq/image/upload/v1749385678/img6_qggjtj.webp",
+                "https://res.cloudinary.com/dvnrlqqpq/image/upload/v1749385678/img5_k0ytpa.webp"
+              ],
+              "description":"Join HOH-7.0, the premier hackathon event for innovators, builders, and dreamers. Compete for prizes, network with industry leaders, and showcase your skills.",
+              "organizer":{
+                "@type":"Organization",
+                "Name":"App Team NITH"
+              },
+              "logo":"https://res.cloudinary.com/dvnrlqqpq/image/upload/v1749389039/hoh3_nqktap.webp",
               "startDate": "2025-10-10T00:00:00Z",
               "endDate": "2025-10-13T23:59:59Z",
               "url": "https://hackonhills.com",
